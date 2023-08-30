1c0b65d3e1cae248b2dd35fcd541a1dc0f7489a5
Matt Cooper <vtbassmatt@gmail.com>
2023-08-30 10:47:10 -0400

    metadata for release

--- flippy/__init__.py
+++ flippy/__init__.py
@@ -1,4 +1,11 @@
+import importlib.metadata
 from flippy.flippy import Flippy
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except:
+    __version__ = "0.0.1-dev"
+
 
 __all__ = [
     Flippy,
